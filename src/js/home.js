cb648f4f28043448358e07eafe7f7003f71bd9db
David Sánchez Cortez <dsanchez@prototype.mx>
2023-11-29 02:56:20 -0600

    feat:card quiz, loader

--- src/js/home.js
+++ src/js/home.js
@@ -2,9 +2,18 @@
 import Swiper from 'swiper';
 import {  Pagination } from 'swiper/modules';
 
-// import Swiper styles
-// import 'swiper/css';
-// import 'swiper/css/pagination';
+
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+let progresoBase =16.66666666666667;
+let ultimoSlide = 5;
+let calificacion=0;
+let quizRespuestas = new Array();
+
+const progresoBar = document.querySelector('.progreso .progress-bar');
+
+
 
 // const mediaquery = window.matchMedia("(max-width:1278px)");
 // const mediaqueryBeneficios = window.matchMedia("(max-width:767px)");
@@ -65,4 +74,74 @@ import {  Pagination } from 'swiper/modules';
 
 // initSliderPromos();
 // initSliderBeneficios();
+
+   const  swiperQuiz = new Swiper(".swiper-quiz", {
+        modules: [Pagination],
+        spaceBetween: "92",
+        slidesPerGroupSkip:'2',
+        direction: "horizontal",
+        allowTouchMove:false,
+        pagination: {
+            el: ".swiper-pagination",
+            type:'fraction'
+        },
+       
+    });
+
+const regresaActions = document.querySelectorAll('.swiper-quiz .pregunta .link-back a');
+regresaActions.forEach( regresa=>{
+    regresa.addEventListener('click',()=>{        
+       
+        progresoBar.style.width= progresoBase + ((swiperQuiz.realIndex - 1) * progresoBase) + "%" ;
+        swiperQuiz.slidePrev(300, true);
+    })
+})
+   
+  
+
+
+const respuestas = document.querySelectorAll('.swiper-quiz .respuestas .respuesta');
+respuestas.forEach(respuesta => {
+    respuesta.addEventListener('click',()=>{
+        // console.log(respuesta.dataset.puntaje);
+        quizRespuestas[swiperQuiz.realIndex] = parseInt( respuesta.dataset.puntaje);
+        if(swiperQuiz.realIndex == ultimoSlide){
+            console.log('Es el ultimo slide: entonces cargar el loader y luego mostrar resultado');            
+            let suCalifiacion = obtenerCalificacion(quizRespuestas);
+            obtenerPerfil(suCalifiacion);
+        }
+        progresoBar.style.width= progresoBase + ((swiperQuiz.realIndex + 1) * progresoBase) + "%";      
+        swiperQuiz.slideNext(300, true);
+    })
+});
+
+// swiperQuiz.on('reachEnd',()=>{
+//     ultimoSlide =true;
+// })
+
+//TODO: funcion para calcular perfil
+const obtenerCalificacion = (respuestasQuiz)=>{
+    calificacion = 0;    
+    respuestasQuiz.forEach( respuesta=>{      
+        calificacion += respuesta;
+    })
+  return calificacion;
+}
+//TODO: funcion para mostrar el perfil en base a la calificacion del perfil
+// Si la puntuación total es menor a 5, la calificación podría ser  Ahorrador.
+// Si la puntuación total está entre 5 y 9, la calificación podría ser  Espontáneo.
+// Si la puntuación total es igual o mayor a 10, la calificación podría ser Generoso.
+function obtenerPerfil(calificacion){
+    
+   let perfil ='';
+    if(calificacion < 5){
+        perfil='ahorrador';
+    }else if( calificacion >= 5 && calificacion <=9){
+        perfil='espontaneo';
+    }else{
+        perfil='generoso';
+    }
+
+   return perfil;
+}
 
